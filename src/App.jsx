42399831e27fe5306a33004ff0422b3ea52bd2f3
Samuel6693 <samuel_somy93@hotmail.com>
2025-12-08 21:57:04 +0100

    Added pages and routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,25 @@
 import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import HomePage from './pages/HomePage'
+import TodosPage from './pages/TodosPage'
+import HabitsPage from './pages/HabitsPage'
+import EventsPage from './pages/EventsPage'
+import Error from './pages/ErrorPage'
 
 function App() {
-
-
   return (
-    <>
-      <HomePage />
-    </>
-  )
+    <BrowserRouter>
+
+      <Routes>
+        <Route path="/" element={<HomePage />}/>
+        <Route path="/todos" element={<TodosPage />}/>
+        <Route path="/habits" element={<HabitsPage />}/>
+        <Route path="/events" element={<EventsPage />}/>
+        <Route path="*" element={<Error />}/>
+      </Routes>
+    
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
