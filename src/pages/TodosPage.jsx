92f74beee9e5dbac5aff1ad35eb629e0903a6082
Samuel6693 <samuel_somy93@hotmail.com>
2025-12-11 14:52:50 +0100

    1- Created full Todo form layout. 2- Implemented controlled inputs using React state 3- Added TodoList array state to store multiple todos 4- Connected submit button to handleSubmit function 5- Added Cancel button to reset form fields 6- Implemented category dropdown with default value 7- Rendered all todos in the “All tasks” section

--- src/pages/TodosPage.jsx
+++ src/pages/TodosPage.jsx
@@ -1,41 +1,153 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 const TodosPage = () => {
+
+    const [todos, setTodos] = useState({
+        title: "",
+        description: "",
+        timeEstimate: "",
+        category: "",
+        deadline: "",
+    });
+
+    const [todoList, setTodoList] = useState([]);
+
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        const newTodo = {
+            id: Date.now(),
+            title:todos.title,
+            description:todos.description,
+            timeEstimate: todos.timeEstimate,
+            category:todos.category,
+            deadline:todos.deadline,
+            status: false,
+        }
+
+        setTodoList((prevTodos) => [...prevTodos, newTodo]);
+
+        setTodos({
+            title: "",
+            description: "",
+            timeEstimate: "",
+            category: "",
+            deadline: "",
+        });
+
+    };
+
+    const handleCancel = () => {
+        setTodos({
+            title: "",
+            description: "",
+            timeEstimate: "",
+            category: "", 
+            deadline:"",
+        });
+    };
+
+    
     return (
         <>
             <section>
-            <h1>Todo Page</h1>
-            <nav>
-                <Link to="/"><h2>Översikt</h2></Link>
-            </nav>
+                <h1>Todo Page</h1>
+
+                <nav>
+                    <Link to="/"><h2>Översikt</h2></Link>
+                </nav>
             </section>
 
             <section>
                 <h2>Nytt ärende</h2>
-                Titel: <input type="text" placeholder="Title" required/> 
-                <br/>
-                Beskrivning: <input type="text" placeholder="Beskriv ditt ärende"/>
-                <br/>
-                Tidsestimat: <input type="time" placeholder="Tidsestimat"/>
-                <br />
-                <label> Kategori: 
-                    <select name="kategori" >
-                        <option value="hälsa">Hälsa</option>
-                        <option value="hushåll">Hushåll</option>
-                        <option value="jobbrelaterat">Jobbrelaterat</option>
-                        <option value="nöje ">Nöje</option>
-                    </select>
-                </label>
-                <br />
-                Deadline: <input type="date" placeholder="Deadline"/>
-                <br />
-                <button>Lägg till</button>
-                <button>Avbryt</button>
+
+                <form onSubmit={handleSubmit}>
+
+
+                    Titel: {" "}
+                    <input 
+                        type="text" 
+                        placeholder="Title" 
+                        required 
+                        value={todos.title}
+                        onChange={(e) => setTodos({...todos, title: e.target.value })
+                        }
+                    /> 
+                    <br/>
+
+                    Beskrivning:{" "}
+                    <input
+                        type="text"
+                        placeholder="Beskriv ditt ärende"
+                        required
+                        value={todos.description}
+                        onChange={(e) =>
+                            setTodos({ ...todos, description: e.target.value })
+                        }
+                    />
+                    <br/>
+
+                    Tidsestimat: {" "}
+                        <input 
+                        type="time" 
+                        placeholder="Tidsestimat"
+                        required
+                        value={todos.timeEstimate}
+                        onChange={(e) => setTodos({...todos, timeEstimate: e.target.value})
+                        }
+                    />
+                    <br />
+
+                    <label> Kategori: {" "}
+                        <select 
+                            name="kategori" 
+                            required
+                            value={todos.category}
+                            onChange={(e) => setTodos({...todos, category: e.target.value})
+                            }>
+                                <option value="">Välj kategori</option>
+                                <option value="hälsa">Hälsa</option>
+                                <option value="hushåll">Hushåll</option>
+                                <option value="jobbrelaterat">Jobbrelaterat</option>
+                                <option value="studier">Studier</option>
+                                <option value="ekonomi">Ekonomi</option>
+                                <option value="nöje">Nöje</option>
+                                <option value="personlig">Personlig</option>
+                                <option value="övrigt ">Övrigt</option>
+                        </select>
+                    </label>
+                    <br />
+                    Deadline: {""}
+                    <input 
+                        type="date" 
+                        placeholder="Deadline"
+                        required
+                        value={todos.deadline}
+                        onChange={(e) => setTodos({...todos, deadline: e.target.value})}/>
+                    <br />
+
+                    <button type="submit">Lägg till</button>
+
+                    <button type="button" onClick={handleCancel}>Avbryt</button>
+                </form>
+
             </section>
 
             <section>
-                <h2>Alla ärenden</h2>
-                {/* lista med todos kommer här */}
+                <h1>Alla ärenden</h1>
+                {todoList.length === 0 && <p>Inga ärenden tillgängliga ännu.</p>}
+                {todoList.map((todo) => (
+                    <div key={todo.id}>    
+                        <h2> Titel: {todo.title}</h2>
+                        <p>Beskrivning: {todo.description}</p>
+                        <p>Tidsestimat: {todo.timeEstimate}</p>
+                        <p>Kategori: {todo.category}</p>
+                        <p>Deadline: {todo.deadline}</p>
+                    </div>)
+                
+                    )}
             </section>
         
         </>
